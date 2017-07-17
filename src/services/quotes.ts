6d24295161e5dd108fcffa1d66453b27aa5b2001
SRatna <adhikariratna52@gmail.com>
2017-07-17 13:10:53 +0545

    quotesService completed

--- src/services/quotes.ts
+++ src/services/quotes.ts
@@ -1,3 +1,22 @@
+import {Quote} from "../data/quote.interface";
 /**
  * Created by osho on 7/17/17.
  */
+export class QuotesService {
+  private favQuotes: Quote[] = [];
+
+  addQuoteToFavorites(quote: Quote) {
+    this.favQuotes.push(quote);
+  }
+
+  removeQuoteFromFavorites(quote: Quote) {
+    const index = this.favQuotes.findIndex((myQuote: Quote) => {
+      return myQuote.id == quote.id;
+    });
+    this.favQuotes.splice(index, 1);
+  }
+
+  getFavoriteQuotes() {
+    return this.favQuotes.slice();
+  }
+}
